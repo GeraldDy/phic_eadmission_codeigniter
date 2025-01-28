bc8635543d6fc5233541e44a9ea3f25c984a7746
Gerald B. <38904252+GeraldDy@users.noreply.github.com>
2025-01-28 10:01:45 +0800

    remove  too many log_messages

--- application/controllers/AdmissionController.php
+++ application/controllers/AdmissionController.php
@@ -10,7 +10,7 @@ class AdmissionController extends CI_Controller {
 		//Receive the POST data
 		$data = $this->input->post('jsonData');
 		$jsonData = json_encode($data);
-		log_message('debug', 'Admission form submitted: ' . $jsonData);
+		
 		$mononym = "";
 		$decodeData = json_decode($jsonData, true);
 		
@@ -21,13 +21,13 @@ class AdmissionController extends CI_Controller {
 		else{
 			$mononym = "N";
 		}
-		log_message('debug', 'patient type' . $decodeData['patient_type']);
+		
 		$AdmDate = new DateTime($decodeData['admission_date']);
 		$pBirthdate = new DateTime($decodeData['p_birthday']);
 		$formattedPBirthdate = $pBirthdate->format('m-d-Y');
 		$formattedAdmDate = $AdmDate->format('m-d-Y');
 
-		log_message('debug', 'mononym ' . $decodeData['p_mononym']);
+	
 		$data_array = array(
 			"hospital_code"=> "H12345",
 			"case_number"=>  "123123123",
@@ -41,7 +41,7 @@ class AdmissionController extends CI_Controller {
 			"p_nationality"=> $decodeData['p_nationality'],
 			"p_contact_number"=> $decodeData['p_contact_number'],
 			"p_email_address"=> $decodeData['p_email_address'],
-			"p_address"=> $decodeData['p_street'].$decodeData['p_barangay'].$decodeData['p_city'].$decodeData['p_city'].$decodeData['p_region'].$decodeData['p_province'].$decodeData['p_zip_code'],
+			"p_address"=> $decodeData['p_street']." ".$decodeData['p_barangay']." ".$decodeData['p_city']." ".$decodeData['p_city']." ".$decodeData['p_region']." ".$decodeData['p_province']." ".$decodeData['p_zip_code'],
 			"patient_type"=> $decodeData['patient_type'],
 			"patient_pin"=> $decodeData['patient_pin'],
 			"m_first_name"=> $decodeData['m_first_name'],
@@ -59,9 +59,6 @@ class AdmissionController extends CI_Controller {
 		);
 
 		log_message('debug', 'Admission form submitted: ' . json_encode($data_array));
-		
-
-
 
 		$apiUrl = '127.0.0.1:8000/submit_admission'; // Replace with the actual API URL
 		$api_key='bb0cad44cc100c6b48af12f14fd09e467a5207e29c5249d48ffed619ad8d7112';
@@ -76,7 +73,6 @@ class AdmissionController extends CI_Controller {
 		$response = curl_exec($ch);
         $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         curl_close($ch);
-		log_message('debug', 'API HTTPCODE: ' . $httpCode);
         if ($httpCode == 200) {
             echo json_encode([
                 'status' => 'success',
